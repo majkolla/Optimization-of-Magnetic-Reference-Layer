85375078e2ff80ebee3e6522abd8df1893501e63
majkolla <michael.birtman@gmail.com>
2025-11-25 11:49:04 +0100

    robustness structure

---
+++ src/solvers/robustness.py
@@ -0,0 +1,12 @@
+class MCRobust: 
+    def __init__(self):
+        pass
+    def __call__(self, *args, **kwds):
+        pass
+
+class WorstCaseRobust: 
+    def __init__(self):
+        pass
+    def __call__(self, *args, **kwds):
+        pass
+    
