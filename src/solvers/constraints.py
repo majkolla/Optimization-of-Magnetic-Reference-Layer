c9ef1ae931d36f6416d7eb18ee62c732a7fcfb6e
majkolla <michael.birtman@gmail.com>
2025-11-25 11:49:31 +0100

    small changes

---
+++ src/solvers/constraints.py
@@ -0,0 +1,16 @@
+
+
+class PenaltyAdapter: 
+    """Wrap an objective and subtract a penalty if it goes outside the 
+        allowed space """
+    def __init__(self):
+        pass
+    def __call__(self, *args, **kwds):
+        pass
+
+class RepairAdapter: 
+    def __init__(self):
+        pass
+    def __call__(self, *args, **kwds):
+        pass
+    
