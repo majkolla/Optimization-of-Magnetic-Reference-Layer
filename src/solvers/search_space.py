4f90aeb0f2e0689124640ff75cbb0772cbd2f127
majkolla <michael.birtman@gmail.com>
2025-12-01 22:19:06 +0100

    created the abstract class for search sapce

--- src/solvers/search_space.py
+++ src/solvers/search_space.py
@@ -1,11 +1,58 @@
 """
-param types cont, discrete etc 
+General parameter and just search space abstractions used by ther opt framework 
+
+- param 
+- cont. param 
+- int param 
+- cat param 
+- search space 
+
+Each parameter is a "scalar" in the opt. vector. And the search space holds the list of the parameters 
+and it provides helping functions to unpack and pack the vals to their numeric vector repr. used by the solver. 
 """
 
-class Param: ... 
+import numpy as np 
+from dataclasses import dataclass, field
+from typing import Any, Dict, Mapping, Sequence, List
 
+
+@dataclass
+class Param:
+    """
+    Base (abstract) class for a single scalar parameter dim
+
+    subclasses: 
+        - pack: Any -> float 
+        - unpack: float -> float
+        - clip(scalar): float -> float 
+        - sample(n): 
+    """
+    name: str 
+
+    def pack(self, value: Any) -> float: 
+        """ Convert a python value to its float representation """
+        raise NotImplementedError
+    
+    def unpack(self, scalar: float) -> Any: 
+        """ Convert a float representation back to a python value """
+        raise NotImplementedError
+    
+    def clip(self, scalar: float) -> float: 
+        """
+        Project a float repr. inot the valid domain 
+        
+        Returns a new float: does not modify the input 
+        """
+        raise NotImplementedError
+
+    def sample(self, n: int = 1) -> np.ndarray: 
+        raise NotImplementedError
+
+@dataclass
 class ContinuousParam(Param): 
-    pass 
+    lo: float
+    hi: float 
+
 
 class IntegerParam(Param): 
     pass 
